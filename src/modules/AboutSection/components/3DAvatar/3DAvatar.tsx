925a816aa5c3992190ffdf61720f6414706c8275
Melik1986 <melikmusinian@gmail.com>
2025-08-12 19:55:06 +0200

    +++++

--- src/modules/AboutSection/components/3DAvatar/3DAvatar.tsx
+++ src/modules/AboutSection/components/3DAvatar/3DAvatar.tsx
@@ -10,24 +10,18 @@ export function Avatar() {
   return (
     <div
       id="avaturn-container"
-      ref={(el) => { refs.current.container = el; }}
-      style={{ width: '100%', height: '100%', position: 'relative' }}
+      ref={(el) => {
+        refs.current.container = el;
+      }}
+      className={styles['avatar-container']}
+      data-animation="slide-right"
+      data-delay="0.5"
+      data-duration="0.8"
+      data-ease="power2.out"
     >
-      <div
-        className={styles["avatar-container"]}
-        id="avaturn-loading"
-        style={{
-          position: 'absolute',
-          top: 10,
-          left: 10,
-          color: '#fff',
-          background: 'rgba(0,0,0,0.5)',
-          padding: '5px 10px',
-          borderRadius: '5px',
-        }}
-      >
+      <div id="avaturn-loading" className={styles['avatar-loading']}>
         Loading...
       </div>
     </div>
   );
-};
+}
